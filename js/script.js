376d743b4cfda6f3f339446a576897ac6a92bd84
Miriam <miriam101d@gmail.com>
2025-09-27 18:35:02 +0200

    preview

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function updateDateTime(){
+    const now=new Date();
+    const options={
+        weekday:'short', year: 'numeric',month:'short',day: 'numeric', hour:'2-digit',minute:'2-digit',second:'2-digit'
+    };
+    const localDateTime=now.toLocaleDateString('es-ES',options);
+    document.getElementById('date-time').textContent=localDateTime;
+}
+
+setInterval(updateDateTime,1000); //se actualiza cada segundo
+updateDateTime();
