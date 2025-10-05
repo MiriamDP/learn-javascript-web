6f6a7e8c8e5a0c91e3bd15782f0070de0e0110a8
Miriam <miriam101d@gmail.com>
2025-10-05 21:42:17 +0200

    ejercicio 6, queda la parte de explicacion

--- ejercicios/DOM/DOM-ejercicios/ejercicio6/js/script.js
+++ ejercicios/DOM/DOM-ejercicios/ejercicio6/js/script.js
@@ -1,16 +1,42 @@
 "use strict";
 
-const idElement=document.getElementById("hola");
+const btnActive=document.getElementById("activeEvent");
+const btnRemove=document.getElementById("removeEvent");
 const resultado=document.getElementById("resultado");
+const raven=document.getElementById("raven");
+const ravenLink=document.getElementById("ravenLink");
 
-console.log(boton);
+btnActive.addEventListener("click",()=>{
+    raven.addEventListener("mouseover",traducir);
+    resultado.textContent="Pasa el raton por encima del texto para traducir";
+});
 
-/*Ojo en las utlimas versiones de los elementos los objetos del DOM con un id pueden 
-aparecer directamente en la variable window por lo que se puede acceder a ellos
-sin necesidad de hacer un getElementById, pero esto es poco recomendable
-Se ha dejado aqui como caso curioso*/
-boton.onclick=mostrar; //esta es una forma de añadir un evento a un elemento, como se asigna la funcion no hay que ponerle los parentesis
+function traducir(){
+    raven.innerHTML=`Una vez, al filo de una lúgubre media noche,
+mientras débil y cansado, en tristes reflexiones embebido,
+inclinado sobre un viejo y raro libro de olvidada ciencia,
+cabeceando, casi dormido,
+oyóse de súbito un leve golpe,
+como si suavemente tocaran,
+tocaran a la puerta de mi cuarto.
+“Es —dije musitando— un visitante
+tocando quedo a la puerta de mi cuarto.
+Eso es todo, y nada más.”`
+    raven.style.color="rgb(163, 38, 96)";
+    ravenLink.href="https://www.literatura.us/idiomas/eap_cuervo.html";
+    ravenLink.innerHTML="Para leerlo completo en español";
+};
 
-function mostrar(){
-    resultado.innerHTML=idElement.textContent;
-}
+btnRemove.addEventListener("click",()=>{
+    raven.removeEventListener("mouseover",traducir);
+    raven.style.color="black";
+    raven.innerHTML=`Once upon a midnight dreary, while I pondered, weak and weary,
+Over many a quaint and curious volume of forgotten lore—
+    While I nodded, nearly napping, suddenly there came a tapping,
+As of some one gently rapping, rapping at my chamber door.
+“’Tis some visitor,” I muttered, “tapping at my chamber door—
+            Only this and nothing more.”`;
+    ravenLink.href="https://www.poetryfoundation.org/poems/48860/the-raven";
+    ravenLink.innerHTML="Para leerlo completo en inglés";
+    resultado.textContent="Leelo otra vez en ingles";
+});
