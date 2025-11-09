70dfd2dee1bcd1765611cca801eb86fea90477a3
Miriam <miriam101d@gmail.com>
2025-11-09 14:57:48 +0100

    no se puede acceder a la api de superhero desde front

--- superhero/js/APIcall.js
+++ superhero/js/APIcall.js
@@ -4,17 +4,6 @@ export async function APIcall(search) {
     const tokenAccess="5827e69a09b59ce2426d3eda804acc0b";
     try {
         let url=urlBase+tokenAccess+"/search/"+search;
-        console.log(url);
-        // if (typeof search=="number"){
-        //     url=urlBase+`/${search}`;
-        // }else{
-        //     if (Object.keys(search).length==1){
-        //         url=urlBase+`?name=${search.name}`;
-        //     }else{
-        //         url=urlBase+`?race=${search.race}&gender=${search.gender}&affiliation=${search.affiliation}`;
-        //     }
-
-        // }
         const data=await APIresponse(url);
         return data;
         
